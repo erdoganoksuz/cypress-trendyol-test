0e86eca50bcfafe2e9ae76b2b09b16d226196e5a
Erdogan Oksuz <erdoganoksuz@Erdogans-MacBook-Pro.local>
2019-03-21 23:10:04 +0300

    init

---
+++ cypress/integration/boutique.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+describe("m.trendyol.com", () => {
+
+    beforeEach(() => {
+        cy.viewport("iphone-6");
+        cy.visit("m.trendyol.com");
+    })
+
+    it("Boutique list should be exist", () => {
+        cy.get(".boutique").should("have.length", 15);
+    });
+
+    it("Boutique detail should be exist", () => {
+        cy.get(".boutique").first().click();
+        cy.get(".product").should("have.length", 20);
+    });
+
+    it("Product images and brand name should be exist", () => {
+        cy.get(".boutique").first().click();
+        cy.get(".product").first().click();
+        cy.get(".gallery__image").should("have.length.greaterThan", 1);
+        cy.get(".product_info__brand").should("exist");
+
+    })
+})
